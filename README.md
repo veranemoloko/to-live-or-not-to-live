0893fc8db89e474ac29f2d8dded20ed723b3731a
Verochka <neveranemoloko@gmail.com>
2025-05-31 16:59:47 +0300

    add rainbow

--- README.md
+++ README.md
@@ -1,11 +1,33 @@
+# Game of Life
+
+A colorful terminal implementation of Conway's Game of Life with rainbow visualization.
+
 ## How It Works?
-# Cells follow 3 rules:
+### Cells follow 3 rules:
 - Survive with 2-3 neighbors
 - Die from loneliness (<2) or overcrowding (>3)
 - Reproduce if exactly 3 neighbors (dead cells come alive)
-- Control: Adjust speed and starting patterns (random/file input)
+- Control: Adjust speed and starting patterns (fields/pattern)
 
-## You need:
-- git clone
-- gcc game_of_life.c -o life && ./life
-- enjoy
+## Features:
+- Adjustable simulation speed 
+- Two input modes:
+    - Random grid generation with customizable density
+    - Loading patterns from text files
+- Toroidal world (edges wrap around)
+- Generation counter and speed display
+- ANSI escape codes for colors and terminal control
+
+## For playing:
+```bash
+git clone https://github.com/yourusername/game-of-life.git
+cd game-of-life
+mkdir build && cd build
+cmake ..
+make
+./life
+```
+
+
+
+![alt text](image-1.png)
--- main.c
+++ main.c
@@ -3,39 +3,69 @@
 #include <time.h>
 #include <unistd.h>
 
+// field size
 #define WIDTH 80
 #define HEIGHT 25
+
 #define MAX_SPEED 5
 #define MIN_SPEED 1
 
-void initField(char field[HEIGHT][WIDTH]);
+#define DELAY 1000000
+#define SPEED_FACTOR 2
+
+// ANSI magic
+#define RAINBOW_COLORS 7
+const char *rainbow[] = {
+    "\033[38;5;196m", // red
+    "\033[38;5;202m", // orange
+    "\033[38;5;226m", // yellow
+    "\033[38;5;46m",  // green
+    "\033[38;5;21m",  // blue
+    "\033[38;5;93m",  // violet
+    "\033[38;5;201m", // pink
+    "\033[38;5;236m"  // grey
+};
+
+void clearScreen() { system("clear || cls"); }
+int input(int *speed, char field[HEIGHT][WIDTH]);
 int setSpeed(int *speed);
-void analysis(char field[HEIGHT][WIDTH]);
-void printField(char field[HEIGHT][WIDTH], int speed, int generation);
-void clearScreen();
-void randomFill(char field[HEIGHT][WIDTH], int density);
 int fileFill(char field[HEIGHT][WIDTH]);
+void randomFill(char field[HEIGHT][WIDTH], int density);
+void updateField(char field[HEIGHT][WIDTH]);
+void printField(char field[HEIGHT][WIDTH], int speed, int generation);
 
 int main() {
   int speed, generation = 0;
   char field[HEIGHT][WIDTH];
 
   printf("Hey, wanna game for a while?\n \
-    Okay, then choose input:\n \
+    Okay, then choose input way:\n \
     1 - random fill\n \
     2 - read from file\n");
 
+  if (input(&speed, field))
+    return 1;
+
+  while (1) {
+    clearScreen();
+    updateField(field);
+    printField(field, speed, ++generation);
+  }
+  return 0;
+}
+
+int input(int *speed, char field[HEIGHT][WIDTH]) {
   int choice;
   scanf("%d", &choice);
 
   switch (choice) {
-  case 1:
-    printf("enter alives count sells(1-100): ");
+  case 1: // random
+    printf("Enter alives count sells (1-100): ");
     int density;
     scanf("%d", &density);
     randomFill(field, density);
     break;
-  case 2:
+  case 2: // file
     if (fileFill(field)) {
       return 1;
     }
@@ -44,40 +74,11 @@ int main() {
     printf("invalid choice. Bye...\n");
     return 1;
   }
-
-  if (setSpeed(&speed)) {
-    printf("enter speed from %d to %d\n", MIN_SPEED, MAX_SPEED);
+  if (setSpeed(speed)) {
+    printf("Enter speed from %d to %d\n", MIN_SPEED, MAX_SPEED);
     return 1;
   }
-
-  clearScreen();
-  while (1) {
-    analysis(field);
-    printField(field, speed, ++generation);
-  }
-
   return 0;
-}
-
-void clearScreen() { printf("\033[2J\033[H"); }
-
-void initField(char field[HEIGHT][WIDTH]) {
-  printf("\033[H\033[J");
-  char c;
-  for (int i = 0; i < HEIGHT; i++) {
-    for (int j = 0; j < WIDTH; j++) {
-      c = (char)fgetc(stdin);
-      if (c == '\n') {
-        j--;
-        continue;
-      } else if (c == EOF) {
-        field[i][j] = '.';
-        continue;
-      }
-      field[i][j] = (c == '*') ? '*' : '.';
-    }
-  }
-  freopen("/dev/tty", "r", stdin);
 }
 
 void randomFill(char field[HEIGHT][WIDTH], int density) {
@@ -90,20 +91,24 @@ void randomFill(char field[HEIGHT][WIDTH], int density) {
 }
 
 int fileFill(char field[HEIGHT][WIDTH]) {
-  printf("say filename: ");
+  printf("Say me filepath: ");
   char filename[256];
   scanf("%s", filename);
+
   FILE *file = fopen(filename, "r");
   if (!file) {
-    printf("error open file. bye...\n");
+    printf("Error open file. Bye...\n");
     return 1;
   }
+
   char c;
   for (int i = 0; i < HEIGHT; i++) {
     for (int j = 0; j < WIDTH; j++) {
       c = (char)fgetc(file);
+
       if (c == '\n' || c == EOF) {
         field[i][j] = '.';
+        // if the file ends earlier, the array will be filled with dots
         if (c == EOF && i < HEIGHT - 1) {
           for (int k = i + 1; k < HEIGHT; k++) {
             for (int l = 0; l < WIDTH; l++) {
@@ -126,44 +131,46 @@ int fileFill(char field[HEIGHT][WIDTH]) {
 int setSpeed(int *speed) {
   printf("Speed (%d-%d): ", MIN_SPEED, MAX_SPEED);
   char test;
-  if (scanf("%d%c", speed, &test) != 2 || test != '\n') {
+  if (scanf("%d%c", speed, &test) != 2 || test != '\n')
     return 1;
-  }
-  if (*speed < MIN_SPEED || *speed > MAX_SPEED) {
+  if (*speed < MIN_SPEED || *speed > MAX_SPEED)
     return 1;
-  }
-  *speed = 1000000 / (*speed * 2);
+  *speed = DELAY / (*speed * SPEED_FACTOR);
   return 0;
 }
 
 void printField(char field[HEIGHT][WIDTH], int speed, int generation) {
   printf("\033[H");
-  printf("..............................THE...GAME...OF...LIFE................."
-         "............\n");
-  printf("Alive: %d | Speed: %d | Ctrl+C to exit\n\n", generation,
-         1000000 / speed / 2);
-
+  printf(
+      "%s..............................THE...GAME...OF...LIFE................."
+      "............\n",
+      rainbow[2]);
+  printf("%sAlive: %d | Speed: %d | Ctrl+C to exit\n", rainbow[3], generation,
+         DELAY / speed / SPEED_FACTOR);
   for (int i = 0; i < HEIGHT; i++) {
     for (int j = 0; j < WIDTH; j++) {
-      printf("%c", field[i][j]);
+      int iColor = (i + j) % RAINBOW_COLORS;
+      if (field[i][j] == '*')
+        printf("%s•\033[0m", rainbow[iColor]);
+      else
+        printf("%s·\033[0m", rainbow[7]);
     }
     printf("\n");
   }
-  printf("\033[J");
   usleep(speed);
 }
 
-void analysis(char field[HEIGHT][WIDTH]) {
-  char field_new[HEIGHT][WIDTH];
+void updateField(char field[HEIGHT][WIDTH]) {
+  char fieldNew[HEIGHT][WIDTH];
   for (int x = 0; x < HEIGHT; x++) {
     for (int y = 0; y < WIDTH; y++) {
       int neighbours = 0;
-
+      // Count live neighbors
       for (int i = -1; i <= 1; i++) {
         for (int j = -1; j <= 1; j++) {
           if (i == 0 && j == 0)
             continue;
-
+          // Toroidal boundary conditions
           int nx = (x + i + HEIGHT) % HEIGHT;
           int ny = (y + j + WIDTH) % WIDTH;
 
@@ -172,18 +179,17 @@ void analysis(char field[HEIGHT][WIDTH]) {
           }
         }
       }
-
       if (field[x][y] == '*') {
-        field_new[x][y] = (neighbours == 2 || neighbours == 3) ? '*' : '.';
+        fieldNew[x][y] = (neighbours == 2 || neighbours == 3) ? '*' : '.';
       } else {
-        field_new[x][y] = (neighbours == 3) ? '*' : '.';
+        fieldNew[x][y] = (neighbours == 3) ? '*' : '.';
       }
     }
   }
-
+  // Update main field with new state
   for (int x = 0; x < HEIGHT; x++) {
     for (int y = 0; y < WIDTH; y++) {
-      field[x][y] = field_new[x][y];
+      field[x][y] = fieldNew[x][y];
     }
   }
 }
